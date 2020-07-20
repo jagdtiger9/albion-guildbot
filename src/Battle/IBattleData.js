beb3896777f8005ca0d9d07b0705421b74183553
cucaracha <cucaracha@mail.ru>
2020-07-20 17:56:39 +0300

    remove Events

--- src/Battle/IBattleData.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * These interfaces define the data structure of the objects returned
- *   by /battles and /battles/:battleId
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=IBattleData.js.map
--- src/Event/IEventData.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * These interfaces define the data structure of the objects returned
- *   by /events and /events/:eventId
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=IEventData.js.map
